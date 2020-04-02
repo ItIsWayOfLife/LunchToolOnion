ad1c0f87080ef607a9de4b0457181d9752db5ad3
ItIsWayOfLife <antongunko1@gmail.com>
2020-04-02 23:23:44 +0300

    transferring constants from views to a shared resources file

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -7,11 +7,13 @@ using Infrastructure.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Globalization;
 using System.IO;
 using Web.Logger;
 
@@ -46,14 +48,32 @@ namespace Web
             services.AddTransient<IOrderService, OrderService>();
             services.AddTransient<IMenuService, MenuService>();
             services.AddTransient<IReportService, ReportService>();
-            
+
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            services.AddControllersWithViews()
+                .AddDataAnnotationsLocalization(options => {
+                    options.DataAnnotationLocalizerProvider = (type, factory) =>
+                        factory.Create(typeof(SharedResource));
+                })
+                .AddViewLocalization();
+
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new[]
+                {
+                    new CultureInfo("ru")
+                };
+
+                options.DefaultRequestCulture = new RequestCulture("ru");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+            });
+
             services.AddControllersWithViews();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
-
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -81,5 +101,6 @@ namespace Web
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
+
     }
 }

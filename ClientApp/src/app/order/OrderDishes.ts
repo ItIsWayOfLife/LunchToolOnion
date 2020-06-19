5356f1e8f6e58fdb151ecd19f9393b891eff74de
Anton Gunko <antongunko1@gmail.com>
2020-06-19 17:45:11 +0300

    add view order

---
+++ ClientApp/src/app/order/OrderDishes.ts
@@ -0,0 +1,11 @@
+export class OrderDishes{
+    public id:number;
+    public count:number;
+    public orderId:number; 
+    public dishId:number;
+    public name:string;
+    public info:string; 
+    public weight:number;
+    public price:number;
+    public path :string;
+}
--- ClientApp/src/app/order/order.component.ts
+++ ClientApp/src/app/order/order.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 
 import {OrderService} from '../service/order.service';
 
 import {Order} from './order';
+import {OrderDishes} from './OrderDishes';
 
 @Component({
   selector: 'app-order',
@@ -10,17 +11,31 @@ import {Order} from './order';
   styleUrls: ['./order.component.css'],
   providers:[OrderService]
 })
-export class OrderComponent implements OnInit {
+export class OrderComponent implements OnInit,
+DoCheck{
 
   orders:Array<Order>;
-ischeckOrderView:boolean;
+  orderDishes:Array<OrderDishes>;
+
+  isViewOrders:boolean;
+  isViewOrdersList:boolean;
+
+  idOrderViewDishes:number;
 
   constructor(private orderServ:OrderService) {
     this.orders = new Array<Order>();
+  
+    this.isViewOrders  = true;
+    this.isViewOrdersList = true;
+    
+    this.orderDishes = new Array<OrderDishes>();
    }
 
   ngOnInit(): void {
    this.loadOrders();
+  }
+
+  ngDoCheck() {
   }
 
     // load menus
@@ -30,6 +45,15 @@ ischeckOrderView:boolean;
           });
   }
 
+  getInfoDish(id:number){
+    this.orderDishes = new Array<OrderDishes>();
+  this.orderServ.getOrderDishById(id).subscribe((data: OrderDishes[]) => {
+    this.orderDishes = data; 
+    this.idOrderViewDishes = data[0].orderId;
+});
+    this.isViewOrdersList = false;
+  }
+
   checkOrderView():boolean{
     if (this.orders.length>0){
       return true;
@@ -37,6 +61,18 @@ ischeckOrderView:boolean;
     else{
       return false;
     }
+  }
+
+  getTotalPrice(orderD:OrderDishes){
+    return (orderD.count * orderD.price).toFixed(2);
+  }
+
+  getFullPrice():number{
+    return this.orders.find(p=>p.id== this.idOrderViewDishes).fullPrice;
+  }
+
+  back(){
+    this.isViewOrdersList = true;
   }
 
 }

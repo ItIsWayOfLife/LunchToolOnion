e3d00d03b642c8ee1ee79c41c9499c70acf92182
Anton Gunko <antongunko1@gmail.com>
2020-06-20 17:20:54 +0300

    add report

--- ClientApp/src/app/report/report.component.ts
+++ ClientApp/src/app/report/report.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { Router } from "@angular/router";
-import { DOCUMENT } from '@angular/common';
 
 import {ReportService} from '../service/report.service';
-
 
 @Component({
   selector: 'app-report',
@@ -14,16 +11,49 @@ import {ReportService} from '../service/report.service';
 export class ReportComponent implements OnInit {
 
   providerId:number;
+  userId:string;
+
+  isViewProviders:boolean;
+  isViewUsers:boolean;
+
+  dateWith:Date;
+  dateTo:Date;
 
   reportFile:File;
 
-  constructor(@Inject(DOCUMENT)private document: Document, private router: Router,private reportServ:ReportService) { }
+  constructor(private reportServ:ReportService) { 
+    this.isViewProviders = false;
+    this.isViewUsers = false;
+  }
 
   ngOnInit(): void {
   }
 
-  getReportProvider(){
-    this.reportServ.getReportProvider(this.providerId).subscribe(
+  dateRefresh(){
+     this.dateTo = new Date();
+     this.dateWith = new Date();
+  }
+
+  viewProviders(){
+    this.dateRefresh();
+    this.isViewProviders = !this.isViewProviders;
+    if (this.isViewProviders){
+      this.isViewUsers = false;
+    }
+  }
+
+  viewUsers(){
+    this.dateRefresh();
+    this.isViewUsers = !this.isViewUsers;
+    if (this.isViewUsers){
+      this.isViewProviders = false;
+    }
+  }
+
+// for providers
+//
+  getReportProviderAllTime(){
+    this.reportServ.getReportProviderAllTime(this.providerId).subscribe(
       (res) => {
         const blob = new Blob([res], {type: "application/pdf"});
         var fileURL = URL.createObjectURL(blob);
@@ -32,4 +62,117 @@ export class ReportComponent implements OnInit {
   );
   }
 
+  getReportProviderDate(){
+    this.reportServ.getReportProviderDate(this.providerId, this.dateWith).subscribe(
+      (res) => {
+        const blob = new Blob([res], {type: "application/pdf"});
+        var fileURL = URL.createObjectURL(blob);
+        window.open(fileURL);
+      }
+  );
+  }
+
+  getReportProviderWithToDate(){
+    this.reportServ.getReportProviderWithToDate(this.providerId, this.dateWith, this.dateTo).subscribe(
+      (res) => {
+        const blob = new Blob([res], {type: "application/pdf"});
+        var fileURL = URL.createObjectURL(blob);
+        window.open(fileURL);
+      }
+  );
+  }
+
+  getReportProvidersAllTime(){
+    this.reportServ.getReportProvidersAllTime().subscribe(
+      (res) => {
+        const blob = new Blob([res], {type: "application/pdf"});
+        var fileURL = URL.createObjectURL(blob);
+        window.open(fileURL);
+      }
+  );
+  }
+
+  getReportProvidersDate(){
+    this.reportServ.getReportProvidersDate(this.dateWith).subscribe(
+      (res) => {
+        const blob = new Blob([res], {type: "application/pdf"});
+        var fileURL = URL.createObjectURL(blob);
+        window.open(fileURL);
+      }
+  );
+  }
+
+  getReportProvidersWithToDate(){
+    this.reportServ.getReportProvidersWithToDate(this.dateWith, this.dateTo).subscribe(
+      (res) => {
+        const blob = new Blob([res], {type: "application/pdf"});
+        var fileURL = URL.createObjectURL(blob);
+        window.open(fileURL);
+      }
+  );
+  }
+//
+
+// for users
+//
+getReportUserAllTime(){
+  this.reportServ.getReportUserAllTime(this.userId).subscribe(
+    (res) => {
+      const blob = new Blob([res], {type: "application/pdf"});
+      var fileURL = URL.createObjectURL(blob);
+      window.open(fileURL);
+    }
+);
+}
+
+getReportUserDate(){
+  this.reportServ.getReportUserDate(this.userId, this.dateWith).subscribe(
+    (res) => {
+      const blob = new Blob([res], {type: "application/pdf"});
+      var fileURL = URL.createObjectURL(blob);
+      window.open(fileURL);
+    }
+);
+}
+
+getReportUserWithToDate(){
+  this.reportServ.getReportUserWithToDate(this.userId, this.dateWith, this.dateTo).subscribe(
+    (res) => {
+      const blob = new Blob([res], {type: "application/pdf"});
+      var fileURL = URL.createObjectURL(blob);
+      window.open(fileURL);
+    }
+);
+}
+
+getReportUsersAllTime(){
+  this.reportServ.getReportUsersAllTime().subscribe(
+    (res) => {
+      const blob = new Blob([res], {type: "application/pdf"});
+      var fileURL = URL.createObjectURL(blob);
+      window.open(fileURL);
+    }
+);
+}
+
+getReportUsersDate(){
+  this.reportServ.getReportUsersDate(this.dateWith).subscribe(
+    (res) => {
+      const blob = new Blob([res], {type: "application/pdf"});
+      var fileURL = URL.createObjectURL(blob);
+      window.open(fileURL);
+    }
+);
+}
+
+getReportUsersWithToDate(){
+  this.reportServ.getReportUsersWithToDate(this.dateWith, this.dateTo).subscribe(
+    (res) => {
+      const blob = new Blob([res], {type: "application/pdf"});
+      var fileURL = URL.createObjectURL(blob);
+      window.open(fileURL);
+    }
+);
+}
+//
 }
